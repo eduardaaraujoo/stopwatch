33305bb1ebfbd32a044f75d85a6ba52d49ade5fa
Eduarda Araujo <dudsaraujo16@gmail.com>
2023-11-11 16:33:45 -0300

    Add the event resumeTimer and resetTimer

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,8 @@ let isPaused = false;
 
 startBtn.addEventListener("click", startTimer);
 pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 function startTimer() {
     interval = setInterval(() => {
@@ -46,6 +48,27 @@ function pauseTimer() {
     isPaused = true
     pauseBtn.style.display = "none";
     resumeBtn.style.display = "block";
+}
+
+function resumeTimer(){
+    isPaused = false;
+    pauseBtn.style.display = "block";
+    resumeBtn.style.display = "none";
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    millisecondsEl.textContent = "000";
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
 }
 
 function formatTime(time) {
